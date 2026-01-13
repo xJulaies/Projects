b012cc89c15af9364c30c142e8ab74d01797022a
julius steffen <xjulaiesx@gmail.com>
2026-01-13 15:26:04 +0100

    Fix: Lightmode localStorage, Banner; Add: bundles.html

--- 002/src/javascript/toggle_theme.js
+++ 002/src/javascript/toggle_theme.js
@@ -7,11 +7,25 @@ toggle_theme_btn.addEventListener("click", () => {
   const theme = html.dataset.theme;
   if (theme === "light") {
     html.dataset.theme = "dark";
-    theme_icon.src = "/002/src/assets/lightmode.png";
-    logo_img.src = "/002/src/assets/bdowhite.png";
+    theme_icon.src = "/Tailwind_projects/002/src/assets/images/lightmode.png";
+    logo_img.src = "/Tailwind_projects/002/src/assets/images/bdowhite.png";
+    localStorage.setItem("theme", "dark");
   } else {
     html.dataset.theme = "light";
-    theme_icon.src = "/002/src/assets/darkmode.png";
-    logo_img.src = "/002/src/assets/bdoblack.png";
+    theme_icon.src = "/Tailwind_projects/002/src/assets/images/darkmode.png";
+    logo_img.src = "/Tailwind_projects/002/src/assets/images/bdoblack.png";
+    localStorage.setItem("theme", "light");
   }
 });
+
+const savedTheme = localStorage.getItem("theme") || "light";
+
+if (savedTheme === "dark") {
+  html.dataset.theme = "dark";
+  theme_icon.src = "/Tailwind_projects/002/src/assets/images/lightmode.png";
+  logo_img.src = "/Tailwind_projects/002/src/assets/images/bdowhite.png";
+} else {
+  html.dataset.theme = "light";
+  theme_icon.src = "/Tailwind_projects/002/src/assets/images/darkmode.png";
+  logo_img.src = "/Tailwind_projects/002/src/assets/images/bdoblack.png";
+}
