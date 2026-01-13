63183748fb1ae2591fbb7e1b47bfc1d92913235e
xJulaies <julius_steffengmx.de>
2026-01-13 10:08:44 +0100

    Fix: Patchnotes

--- 002/src/javascript/toggle_theme.js
+++ 002/src/javascript/toggle_theme.js
@@ -1,18 +1,17 @@
 const html = document.querySelector("html");
 const toggle_theme_btn = document.querySelector(".toggle-theme-btn");
 const theme_icon = document.querySelector(".theme-icon");
-const logo = document.querySelector(".logo");
+const logo_img = document.querySelector(".logo-img");
 
 toggle_theme_btn.addEventListener("click", () => {
   const theme = html.dataset.theme;
   if (theme === "light") {
     html.dataset.theme = "dark";
-    theme_icon.src = "src/assets/lightmode.png";
-    logo.src = "src/assets/bdowhite.png";
+    theme_icon.src = "/002/src/assets/lightmode.png";
+    logo_img.src = "/002/src/assets/bdowhite.png";
   } else {
     html.dataset.theme = "light";
-    theme_icon.src = "src/assets/darkmode.png";
-    logo.src = "src/assets/bdoblack.png";
+    theme_icon.src = "/002/src/assets/darkmode.png";
+    logo_img.src = "/002/src/assets/bdoblack.png";
   }
-  console.log(html.dataset.theme);
 });
