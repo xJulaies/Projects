34dcd020a72f2edf7f09d2cc8a88705ba069b821
xjulaies <julius_steffen@gmx.de>
2026-02-13 23:09:55 +0100

    Add: dataset IDs

--- Project_Game_Center/scripts/boards.js
+++ Project_Game_Center/scripts/boards.js
@@ -14,9 +14,13 @@ export class createBoards {
       for (let cellIndex = 0; cellIndex < 9; cellIndex++) {
         const cell = document.createElement("div");
         cell.classList.add("inner-squares");
+        cell.dataset.row = Math.floor(cellIndex / 3);
+        cell.dataset.col = Math.floor(cellIndex % 3);
+        cell.dataset.block = squareIndex;
         for (let notesIndex = 0; notesIndex < 9; notesIndex++) {
           const noteSquare = document.createElement("div");
           noteSquare.classList.add("note-square");
+          noteSquare.dataset.notesId = notesIndex;
           cell.append(noteSquare);
         }
         square.append(cell);
