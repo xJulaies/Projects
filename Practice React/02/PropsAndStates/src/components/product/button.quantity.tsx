03e1216fb1daa3c088a1d35b0b7f9d0c7cecc0d7
xjulaies <julius_steffen@gmx.de>
2026-05-29 17:08:48 +0200

    Fix button.quantity.tsx

--- Practice React/02/PropsAndStates/src/components/product/button.quantity.tsx
+++ Practice React/02/PropsAndStates/src/components/product/button.quantity.tsx
@@ -3,7 +3,6 @@ import type { TQuantityButtonProps } from "../../types/button.types";
 export function QuantityButton({ onClick, text }: TQuantityButtonProps) {
   return (
     <>
-      {" "}
       <div className="flex flex-col gap-2">
         <button onClick={onClick} className="btn btn-xs">
           {text}
