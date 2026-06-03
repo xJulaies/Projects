ea2ef835b58a9eb0f84447d573a0929c7c6df6be
xjulaies <julius_steffen@gmx.de>
2026-06-03 16:02:40 +0200

    Add errorhandling backend

--- Practice React/05/Backend/src/index.ts
+++ Practice React/05/Backend/src/index.ts
@@ -1,10 +1,25 @@
 import express, { json, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { settings } from "./config/settings";
+import { createError } from "./lib/handleError/createError";
+import { createAnswer } from "./lib/handleError/createAnswer";
+import type { TCreateError } from "./types/errorTypes";
 
 const app = express();
 
 app.use(json());
+
+app.use((_req: Request, _res: Response, next: NextFunction) => {
+  return next(createError(404, "Not found"));
+});
+
+app.use(
+  (err: TCreateError, req: Request, res: Response, next: NextFunction) => {
+    return res
+      .status(err.status || 500)
+      .json(createAnswer(err.status || 500, err.message || "Server Error", []));
+  },
+);
 
 app.listen(settings.PORT, () => {
   console.log(`Server Booted at Port ${settings.PORT}`);
