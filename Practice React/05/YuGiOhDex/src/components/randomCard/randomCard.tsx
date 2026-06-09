5ff9eaeee95f9777f8be7af231db4cb3383c0e6f
xjulaies <julius_steffen@gmx.de>
2026-06-09 14:08:15 +0200

    Add custom hook useRandomCard, cleaned up randomCard.tsx

--- Practice React/05/YuGiOhDex/src/components/randomCard/randomCard.tsx
+++ Practice React/05/YuGiOhDex/src/components/randomCard/randomCard.tsx
@@ -1,24 +1,8 @@
-import { useState, useEffect } from "react";
-import { getRandomCard } from "../../lib/api/getRandomCard";
 import { DisplayRandomCard } from "./display.randomCard";
-import type { TCard } from "../../types/card.types";
+import { useRandomCard } from "../../hooks/useRandomCard";
 
 export function RandomCard() {
-  const [card, setCard] = useState<TCard | null>(null);
-
-  useEffect(() => {
-    async function loadRandomCard() {
-      const result = await getRandomCard();
-      setCard(result.data[0]);
-    }
-    loadRandomCard();
-    const intervalId = setInterval(() => {
-      loadRandomCard();
-    }, 10000);
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
+  const { card } = useRandomCard();
 
   return <>{card && <DisplayRandomCard card={card} />}</>;
 }
