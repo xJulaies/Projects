b3bc003f7adbc278a06cbbf71f80f08642eed8fd
xjulaies <julius_steffen@gmx.de>
2026-06-12 14:46:43 +0200

    add basepath

--- Practice React/05/YuGiOhDex/src/App.tsx
+++ Practice React/05/YuGiOhDex/src/App.tsx
@@ -5,7 +5,10 @@ import { routeTree } from "./routeTree.gen";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  basepath: import.meta.env.BASE_URL.replace(/\/$/, ""),
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
