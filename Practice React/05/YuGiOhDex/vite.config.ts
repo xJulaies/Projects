54bc3b355f9ee1ce5384580c83edba9c7b49c47f
xjulaies <julius_steffen@gmx.de>
2026-06-12 14:51:24 +0200

    update build

--- Practice React/05/YuGiOhDex/vite.config.ts
+++ Practice React/05/YuGiOhDex/vite.config.ts
@@ -5,7 +5,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Projects/",
+  base: "/Projects/Practice%20React/05/YuGiOhDex/",
   plugins: [
     tanstackRouter({
       target: "react",
